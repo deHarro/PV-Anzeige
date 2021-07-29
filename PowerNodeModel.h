ebffefbb748f89ad629e654aad9587ce5b877250
deHarro <harry0001@satharo.de>
2021-07-29 12:39:06 +0200

    tweaked some colors

--- PowerNodeModel.h
+++ PowerNodeModel.h
@@ -5,13 +5,19 @@
 #include <QString>
 #include <QQuickImageProvider>
 
+#define DEMOMODE                // generate random power values for coloring and arrows
+
 // define colors according https://doc.qt.io/qt-5/qml-color.html
-#define VLIGHTGRAY      "LIGHTGREY"   // "0xb3b3b3"    // no defined color, hand made ;-)
-#define LIMEGREEN       "LIMEGREEN"   // "0x32cd32"
-#define FORESTGREEN     "FORESTGREEN" // "0x228b22"
+// use #RRGGBB notation for QML instead of 0xRRGGBB
+#define VLIGHTGRAY      "#b3b3b3"     // "LIGHTGREY"    //  no defined color, hand tuned ;-)
+//#define LIMEGREEN       "LIMEGREEN"   // "0x32cd32"     // -> "#32cd32" replace 0x by # for QColor in QML file
+#define LIMEGREEN       "#00ac00"   // "0x32cd32"     // -> "#32cd32" replace 0x by # for QColor in QML file
+#define FORESTGREEN     "FORESTGREEN" // "0x228b22"     // "0x228b22"
 #define FIREBRICK       "FIREBRICK"   // "0xb22222"
-#define DODGERBLUE      "DODGERBLUE"  // "0x0A7CEB"    // EV charging // 0x0A7CEB = "um -20 dunkleres dogerblue" (Orig. 0x1e90ff)
-#define DARKBLUE        "DARKBLUE"    // "0x00008b"    // EV attached to wallbox
+//#define DODGERBLUE      "#0A7CEB"     // "DODGERBLUE"   // EV charging // 0x0A7CEB = "um -20 dunkleres dogerblue" (Orig. 0x1e90ff)
+#define DODGERBLUE      "#02a4f5"     // "DODGERBLUE"   // EV charging // 0x0A7CEB = "um -20 dunkleres dogerblue" (Orig. 0x1e90ff)
+//#define DARKBLUE        "DARKBLUE"    // "0x00008b"     // EV attached to wallbox
+#define DARKBLUE        "#0371da"    // "0x00008b"     // EV attached to wallbox
 
 
 class PowerNodeModel : public QObject {
@@ -29,7 +35,7 @@ public:
     Q_PROPERTY(QString generatorColor MEMBER m_generatorColor NOTIFY generatorDataChanged)
 
     // battery properties - all battery values are updated in one call to "batteryDataChanged"
-    Q_PROPERTY(double batteryPower MEMBER m_batteryPower NOTIFY batteryDataChanged)
+    Q_PROPERTY(double batteryPower MEMBER m_battPowerAnzeige NOTIFY batteryDataChanged)
     Q_PROPERTY(double batteryPercentage MEMBER m_batteryPercentage NOTIFY batteryDataChanged)
     Q_PROPERTY(QString batteryText MEMBER m_batteryText NOTIFY batteryDataChanged)    
     Q_PROPERTY(QString batteryColor MEMBER m_batteryColor NOTIFY batteryDataChanged)
@@ -41,10 +47,10 @@ public:
     Q_PROPERTY(QString homeColor MEMBER m_homeColor NOTIFY consumptionDataChanged)
 
     // grid properties - all grid values are updated in one call to "gridDataChanged"
-    Q_PROPERTY(double gridPower MEMBER m_gridPower NOTIFY gridDataChanged)
+    Q_PROPERTY(double gridPower MEMBER m_gridPowerAnzeige NOTIFY gridDataChanged)
     Q_PROPERTY(double gridEnergyImport MEMBER m_gridEnergyImport NOTIFY gridDataChanged)                    // Verbrauchszähler [kWh]
     Q_PROPERTY(double gridEnergyExport MEMBER m_gridEnergyExport NOTIFY gridDataChanged)                    // Einspeisezähler [kWh]
-    Q_PROPERTY(QString gridText MEMBER m_gridText NOTIFY batteryDataChanged)
+    Q_PROPERTY(QString gridText MEMBER m_gridText NOTIFY gridDataChanged)
     Q_PROPERTY(QString gridColor MEMBER m_gridColor NOTIFY gridDataChanged)
 
     // wallbox properties - all wallbox values are updated in one call to "chargingDataChanged"
@@ -90,6 +96,7 @@ private:
     QString m_generatorColor = VLIGHTGRAY;
 // battery, Akku
     double m_batteryPower = 0.0;            // Batterieladung/-Entladung [kW]
+    double m_battPowerAnzeige = 0.0;        // Batterieladung/-Entladung zur Anzeige [kW]
     int m_batteryPercentage = 0;            // Batterie Ladezustand [%]
     QString m_batteryColor = "";
     QString m_batteryText = "";             // Text in der Batterie Box, wechselt Ladung/Entladung
@@ -100,6 +107,7 @@ private:
     QString m_homeColor = VLIGHTGRAY;
 // grid, Netz
     double m_gridPower = 0.0;               // Netzbezug/Einspeisung [kW]
+    double m_gridPowerAnzeige = 0.0;        // Netzbezug für die Anzeige
     double m_gridEnergyImport = 0.0;        // Zähler [kWh]
     double m_gridEnergyExport = 0.0;        // Einspeisezähler [kWh]
     QString m_gridColor = VLIGHTGRAY;
