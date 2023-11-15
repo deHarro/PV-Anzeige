393b1c3fce425a1dcb5a512e79807ec134d698f1
deHarro <harry0001@satharo.de>
2023-11-15 15:13:13 +0100

    added handling for DachN converter

--- WechselrichterJSON.cpp
+++ WechselrichterJSON.cpp
@@ -16,6 +16,7 @@ void WechselrichterJSON::ReadWechselrichterJSON() {
 
     if(!m_JSONfiledata.isEmpty())
     {
+// WR Garage
         if ((position = m_JSONfiledata.indexOf("SUNSPEC1")) != -1)        // "SUNSPEC1" gefunden
         {
             position = m_JSONfiledata.indexOf("\"Export", position);
@@ -45,6 +46,7 @@ void WechselrichterJSON::ReadWechselrichterJSON() {
             m_messageFlag |= WRFlag;
         }
 
+// WR DachS
         if ((position = m_JSONfiledata.indexOf("SUNSPEC2")) != -1)        // "SUNSPEC2" gefunden
         {
             position = m_JSONfiledata.indexOf("\"Export", position);
@@ -65,15 +67,16 @@ void WechselrichterJSON::ReadWechselrichterJSON() {
                 double dPower = power.toDouble();
                 qDebug() << "Power2: " << dPower;
 
-                m_PVDachActualPower = dPower;
+                m_PVDachSActualPower = dPower;
             }
         }
         else
         {
-            qDebug() << "Wechselrichter Dach ist ausgefallen";
+            qDebug() << "Wechselrichter DachS ist ausgefallen";
             m_messageFlag |= WRFlag;
         }
 
+// WR Gaube
         if ((position = m_JSONfiledata.indexOf("SUNSPEC3")) != -1)        // "SUNSPEC3" gefunden
         {
             position = m_JSONfiledata.indexOf("\"Export", position);
@@ -109,15 +112,59 @@ void WechselrichterJSON::ReadWechselrichterJSON() {
             qDebug() << "Wechselrichter Gaube ist ausgefallen";
             m_messageFlag |= WRFlag;
         }
+
+#ifdef WRDACHN
+// WR DachN
+        if ((position = m_JSONfiledata.indexOf("SUNSPEC4")) != -1)        // "SUNSPEC4" gefunden
+        {
+            position = m_JSONfiledata.indexOf("\"Export", position);
+            positionColon = m_JSONfiledata.indexOf(":", position);
+            // if converters deliver 0 (zero), there is no "comma "," after "Export:", it's a "}"
+            if (m_JSONfiledata.indexOf(",", positionColon) != -1)
+            {
+                positionComma = std::min(m_JSONfiledata.indexOf(",", positionColon), m_JSONfiledata.indexOf("}", positionColon));
+            }
+            else
+            {
+                positionComma = m_JSONfiledata.indexOf("}", positionColon);
+            }
+            QString exportDachN = m_JSONfiledata.mid(positionColon + 1, positionComma - positionColon - 1);
+
+            qDebug() << "exportDachN: " << exportDachN;
+            m_PVGesamtErtrag += exportDachN.toDouble();
+
+            if((position = m_JSONfiledata.indexOf("\"Power", position)) != -1)
+            {
+                positionColon = m_JSONfiledata.indexOf(":", position);
+                positionComma = m_JSONfiledata.indexOf(",", positionColon);
+                QString power = m_JSONfiledata.mid(positionColon + 1, positionComma - positionColon - 1);
+
+                double dPower = power.toDouble();
+                qDebug() << "Power4: " << dPower;
+
+                m_PVDachNActualPower = dPower;
+            }
+        }
+        else
+        {
+            qDebug() << "Wechselrichter DachN ist ausgefallen";
+            m_messageFlag |= WRFlag;
+        }
+#endif
         // m_messageFlag |= WRFlag;                 // Test für Debug!!
     }
 }
 
 WechselrichterJSON::~WechselrichterJSON() {}
 
-double WechselrichterJSON::getPVDachActualPower(void)
+double WechselrichterJSON::getPVDachSActualPower(void)
 {
-    return m_PVDachActualPower;
+    return m_PVDachSActualPower;
+};
+
+double WechselrichterJSON::getPVDachNActualPower(void)
+{
+    return m_PVDachNActualPower;
 };
 
 double WechselrichterJSON::getPVGaubeActualPower(void)
--- WechselrichterJSON.h
+++ WechselrichterJSON.h
@@ -6,6 +6,9 @@
 #include <qjsonobject.h>
 #include <qjsonarray.h>
 
+// WR DachN in Verabrbeitung aufnehmen (raus nehmen durch auskommentieren)
+//#define WRDACHN
+
 // global flag memory for error messages
 extern quint8 m_messageFlag;
 
@@ -19,13 +22,15 @@ public:
     void ReadWechselrichterJSON(void);
 
     // getter functions
-    double getPVDachActualPower(void);
+    double getPVDachSActualPower(void);
+    double getPVDachNActualPower(void);
     double getPVGaubeActualPower(void);
     double getPVGarageActualPower(void);
     double getPVGesamtErtrag(void);
 
 private:
-    double m_PVDachActualPower = 0.0;
+    double m_PVDachSActualPower = 0.0;
+    double m_PVDachNActualPower = 0.0;
     double m_PVGaubeActualPower = 0.0;
     double m_PVGarageActualPower = 0.0;
     double m_PVGesamtErtrag = 0.0;
